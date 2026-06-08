3777642d521cb370c89e8307fbb9f555f2e16113
ententefm <entente.fm@gmail.com>
2026-06-08 13:36:02 +0530

    Update connections.default.js

--- data/connections.default.js
+++ data/connections.default.js
@@ -4,7 +4,7 @@ HUNT.register('connections.default', {
   lives: 5,
   groups: [
     { category:"OUR CATS",        color:"yellow", words:["LazyBum","Naughty","Zoomy","Queen"] },
-    { category:"GAMES YOU PLAY",  color:"green",  words:["GENSHIN","CROSSPLAY","JIGSAW","WORDLE"] },
+    { category:"GAMES YOU PLAY",  color:"green",  words:["GENSHIN","CROSSPLAY","JIGSAW","RUBIKS"] },
     { category:"Our Most Memorable Trips",  color:"blue",   words:["Koh Chang","Hanoi","Brooklyn","Central Park Zoo"] },
     { category:"Your Most Fav Boi",       color:"purple", words:["CALICO","GENTLEST","FLUFFIEST","LIL BOY"] }
   ]
